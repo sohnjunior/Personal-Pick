8b9fd9df3ebd66e5d2d6a8cad61eb61d4e63b9dc
handhand <sohnjunior@naver.com>
2020-02-12 16:19:19 +0900

    shopping DB migrate

---
+++ PersonalPick/shopping/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=64)
+    link = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
+    mallName = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
---
+++ PersonalPick/shopping/utils.py
@@ -0,0 +1,5 @@
+
+
+# 주기적으로 쇼핑 데이터를 업데이트 한 후 DB에 저장한다.
+def update_shopping_data():
+    pass
