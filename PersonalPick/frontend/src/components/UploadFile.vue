e8053168d6e772a238139a88ca3202d4f54fa9ea
handhand <sohnjunior@naver.com>
2020-02-25 19:10:16 +0900

    preview image logic change

--- PersonalPick/frontend/src/components/UploadFile.vue
+++ PersonalPick/frontend/src/components/UploadFile.vue
@@ -3,30 +3,52 @@
     <div class="file-upload-form">
       <b-form-file v-model="imageData" id="file" ref="file" browse-text="파일선택" @change="handleFileUpload"></b-form-file>
     </div> <br>
-    <div class="preview-image">
-      <img v-bind:src="imagePreview"/>
-      <!--
-      <b-img thumbnail alt="사용자 입력 이미지" blank="true" width="300" height="300" rounded="true" v-bind:src="imagePreview"></b-img>
-      -->
-    </div> <hr>
-    <div class="input-button">
+    <div>
+      <b-img thumbnail :src="imagePreview" class="preview-image" block rounded></b-img>
+    </div> <br>
+    <div>
       <b-button pill variant="outline-secondary" size="lg" type="submit" @click="submitFile">결과보기</b-button>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+import axios from 'axios'
 
-  axios.defaults.xsrfCookieName = 'csrftoken';
-  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+const base64Encode = data =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
 export default {
   data() {
     return {
-      imageData: "",
-      imagePreview: ""
+      imageData: null,
+      imagePreview: null
     }
+  },
+  watch: {
+      imageData(newValue, oldValue) {
+        if(newValue !== oldValue) {
+          if(newValue) {
+            base64Encode(newValue)
+            .then(value => {
+              this.imagePreview = value;
+            })
+            .catch(() => {
+              this.imagePreview = null;
+            });
+          } else {
+            this.imagePreview = null;
+          }
+        }
+      }
   },
   methods: {
     submitFile() {
@@ -43,13 +65,6 @@ export default {
     },
     handleFileUpload() {
       this.imageData = this.$refs.file.files[0];
-      
-      // TODO handle preview image
-      const reader = new FileReader();
-      const vm = this;
-
-      reader.onload = (e) => { vm.imagePreview = e.target.result; };
-      reader.readAsDataURL(this.imageData);
     }
   }
 }
@@ -58,6 +73,11 @@ export default {
 <style scoped>
 .file-upload-form {
   width: 25%;
+  margin: auto;
+}
+.preview-image {
+  width: 300px;
+  height: 300px;
   margin: auto;
 }
 </style>
