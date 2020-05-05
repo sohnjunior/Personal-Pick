1000f7a824a820e64b9e39f83b03906ea8007167
handhand <sohnjunior@naver.com>
2020-05-05 14:03:18 +0900

    heroku config

--- backend/shopping/apps.py
+++ backend/shopping/apps.py
@@ -8,23 +8,23 @@ class ShoppingConfig(AppConfig):
     name = 'shopping'
     model_params = None
 
-    # def ready(self):
-    #     """
-    #     앱 초기화시 필요한 작업들을 수행한다.
-    #
-    #     :return: None
-    #     """
-    #     # create s3 client
-    #     s3client = boto3.client('s3',
-    #                             aws_access_key_id=settings.S3_ACCESS_KEY,
-    #                             aws_secret_access_key=settings.S3_SECRET_KEY)
-    #
-    #     # get objects from s3 /w hints
-    #     response = s3client.get_object(Bucket='personal-pick', Key='model/deep.pt')
-    #     print('load necessary files from s3')
-    #
-    #     # read data from s3
-    #     body = response['Body'].read()
-    #     stream = BytesIO(body)
-    #     ShoppingConfig.model_params = stream
-    #     print(f'read model params data from s3')
+    def ready(self):
+        """
+        앱 초기화시 필요한 작업들을 수행한다.
+
+        :return: None
+        """
+        # create s3 client
+        s3client = boto3.client('s3',
+                                aws_access_key_id=settings.S3_ACCESS_KEY,
+                                aws_secret_access_key=settings.S3_SECRET_KEY)
+
+        # get objects from s3 /w hints
+        response = s3client.get_object(Bucket='personal-pick', Key='model/deep.pt')
+        print('load necessary files from s3')
+
+        # read data from s3
+        body = response['Body'].read()
+        stream = BytesIO(body)
+        ShoppingConfig.model_params = stream
+        print(f'read model params data from s3')
