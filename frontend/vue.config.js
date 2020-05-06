e5cf05e0107e7d9b79d289500cc496d773ce549e
handhand <sohnjunior@naver.com>
2020-05-07 02:26:51 +0900

    aws ec2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
-const path = require("path");
-
-module.exports = {
-  outputDir: path.resolve(__dirname, "../config/templates"),
-  assetsDir: "../static"
-}
+// 빌드 파일 생성 경로 지정할때만 사용하기
+// const path = require("path");
+//
+// module.exports = {
+//   outputDir: path.resolve(__dirname, "../config/templates"),
+//   assetsDir: "../static"
+// }
